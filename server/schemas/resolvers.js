c277cc9380ce64c41ebb06a7151de18e5db15642
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-09-23 16:14:55 -0700

    finished moduloe but the success page still isn't working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -53,6 +53,7 @@ const resolvers = {
       throw new AuthenticationError("Not logged in");
     },
     checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referer).origin;
       const order = new Order({ products: args.products });
       const { products } = await order.populate("products").execPopulate();
       const line_items = [];
@@ -62,6 +63,7 @@ const resolvers = {
         const product = await stripe.products.create({
           name: products[i].name,
           description: products[i].description,
+          images: [`${url}/images/${products[i].image}`]
         });
 
         // generate price id using the product id
@@ -77,13 +79,13 @@ const resolvers = {
           quantity: 1,
         });
       }
+
       const session = await stripe.checkout.sessions.create({
         payment_method_types: ["card"],
         line_items,
         mode: "payment",
-        success_url:
-          "https://example.com/success?session_id={CHECKOUT_SESSION_ID}",
-        cancel_url: "https://example.com/cancel",
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}`,
       });
 
       return { session: session.id };
